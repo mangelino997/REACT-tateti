44f68ad78cefc65f80ba3980a389f382972d1d1d
Marcio <marcio@draimo.com>
2020-09-03 18:54:43 -0300

    Version final con correcciones en el footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,13 @@
 import React, { Fragment } from 'react';
-import logoGit from '../assets/gato.png';
-import logoLk from '../assets/linkedin.png';
+import myWeb from '../assets/global.png';
 const Footer = (prop) => {
     return (
         <Fragment>
             <div className="footer">
-                <div className="footer-copyright text-center py-3">
-                    <span>Marcio Angelino</span>
-                    <a href="https://github.com/mangelino997" target="_blank">
-                        <img src={logoGit}></img>
-                 </a>
-                    <a href="https://www.linkedin.com/in/marcio-angelino-069592186/" target="_blank">
-                        <img src={logoLk}></img>
+                <div className="footer-copyright text-center py-1">
+                    <a href="https://mangelino997.github.io/my-web/" 
+                    target="_blank" className="my-web">
+                    My Website   <img src={myWeb}></img>
                  </a>
                 </div>
             </div>
